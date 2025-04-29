94199cf374161dd8f70908204620d44be8f20a31
Sreerama MP <97295813+sreeramamp123@users.noreply.github.com>
2025-04-29 18:00:47 +0530

    Commit 2
    Stopwatch Timer Logic Completed

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -8,6 +9,12 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  int milliseconds = 0;
+  int seconds = 0;
+  int minutes = 0;
+  int hours = 0;
+  Timer? _timer;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,25 +22,105 @@ class _HomeState extends State<Home> {
       body: Column(
         children: [
           // Stopwatch Place
-          Expanded(flex: 2, child: Row()),
+          Expanded(
+            flex: 2,
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Row(
+                children: [
+                  Text(
+                    hours.toString().padLeft(2, '0'),
+                    style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(width: 10),
+                  Text(
+                    ":",
+                    style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(width: 10),
+                  Text(
+                    minutes.toString().padLeft(2, '0'),
+                    style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(width: 10),
+                  Text(
+                    ":",
+                    style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(width: 10),
+                  Text(
+                    seconds.toString().padLeft(2, '0'),
+                    style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(width: 10),
+                  Text(
+                    ":",
+                    style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(width: 10),
+                  Text(
+                    milliseconds.toString().padLeft(3, '0'),
+                    style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ),
+          ),
           // Control Buttons Place
           Expanded(
             flex: 2,
-            child: Row(
-              children: [
-                ElevatedButton(
-                  onPressed: () {},
-                  child: Icon(Icons.play_arrow_outlined),
-                ),
-                ElevatedButton(
-                  onPressed: () {},
-                  child: Icon(Icons.stop_circle_sharp),
-                ),
-                ElevatedButton(
-                  onPressed: () {},
-                  child: Icon(Icons.restart_alt),
-                ),
-              ],
+            child: Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  FloatingActionButton(
+                    onPressed: () {
+                      if (_timer == null || _timer?.isActive != true) {
+                        _timer = Timer.periodic(Duration(milliseconds: 1), (
+                          Timer timer,
+                        ) {
+                          setState(() {
+                            milliseconds++;
+                            if (milliseconds == 1000) {
+                              seconds++;
+                              milliseconds = 0;
+                            }
+                            if (seconds == 60) {
+                              minutes++;
+                              seconds = 0;
+                            }
+                            if (minutes == 60) {
+                              hours++;
+                              minutes = 0;
+                            }
+                          });
+                        });
+                      }
+                    },
+                    child: Icon(Icons.play_arrow_outlined, size: 40),
+                  ),
+                  FloatingActionButton(
+                    onPressed: () {
+                      setState(() {
+                        _timer?.cancel();
+                      });
+                    },
+                    child: Icon(Icons.pause, size: 40),
+                  ),
+                  FloatingActionButton(
+                    onPressed: () {
+                      setState(() {
+                        milliseconds = 0;
+                        seconds = 0;
+                        minutes = 0;
+                        hours = 0;
+                      });
+                    },
+                    child: Icon(Icons.restart_alt, size: 40),
+                  ),
+                ],
+              ),
             ),
           ),
         ],
